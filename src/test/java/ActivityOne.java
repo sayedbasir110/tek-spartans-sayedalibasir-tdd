105f0c97096b449599ec0f11774256d951f2e5fb
sayedbasir110 <basir.sayedali@gmail.com>
2024-08-12 18:05:58 -0700

    activityOne

---
+++ src/test/java/ActivityOne.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ActivityOne {
+    WebDriver driver;
+    @BeforeMethod
+    public void beforeEachTest (){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+    @AfterMethod
+    public void afterEachTest(){
+        driver.quit();
+    }
+    @Test
+    public void TestOne(){
+        driver.get("https://facebook.com");
+        String title= driver.getTitle();
+        System.out.println(title);
+    }
+}

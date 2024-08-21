72d192f63c922f3de97c6e80221062ca2bd5f133
sayedbasir110 <basir.sayedali@gmail.com>
2024-08-21 16:42:36 -0700

    activityOne

--- src/test/java/tek/tdd/tests/UpdateAccountInfoTest.java
+++ src/test/java/tek/tdd/tests/UpdateAccountInfoTest.java
@@ -1,20 +1,28 @@
 package tek.tdd.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import tek.tdd.base.UIBaseClass;
 
 public class UpdateAccountInfoTest extends UIBaseClass {
-    @Test
-    public void updateAccountInfoAndValidate(){
+    @Test(dataProvider = "accountInfoData")
+    public void updateAccountInfoAndValidateChanges(String name, String phoneNumber){
         homePage.clickOnSignIn();
         signInPage.doSignIn("john.doe@gmail.com","John@123");
         homePage.clickOnAccountLink();
-        accountPage.updateAccountNameAndPhoneNumber("Ali", "2365918255");
+        accountPage.updateAccountNameAndPhoneNumber(name, phoneNumber);
         boolean isToastMessageDisplayed = isElementDisplayed(accountPage.toastMessage);
-        Assert.assertTrue(isToastMessageDisplayed);
-        Assert.assertEquals(accountPage.getAccountName(),"Ali", "Account name should match");
+        Assert.assertTrue(isToastMessageDisplayed, "Toast message should be displayed after updating account info");
+        Assert.assertEquals(accountPage.getAccountName(),name, "Account name should match");
         accountPage.updateAccountNameAndPhoneNumber("John Doe", "5152568974");
-
+    }
+    @DataProvider(name = "accountInfoData")
+    public String[][] accountInfoData(){
+        return new String[][]{
+                {"Ali", "2365918255"},
+                {"Eric", "6041582565"},
+                {"Michelle", "2024518796"},
+        };
     }
 }
